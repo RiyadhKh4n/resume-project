3c2530b2eb81be216cab93fcc680d309a7c98147
Arkay <riyadh.k@outlook.com>
2022-02-14 17:09:58 +0000

    change loader

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -8,7 +8,7 @@ function fetchGitHubInformation(event){
 
     $("#gh-user-data").html(
         `<div id="loader">
-            <img src="assets/css/Infinity-1s-200px.gif" alt="loading"/>
+            <img src="assets/css/infinity.gif" alt="loading"/>
         </div>`
     );
 
